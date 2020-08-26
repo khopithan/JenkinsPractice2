74388661f7aa0e239567d6f11b2ea02eaed34b1b
Khopithan Sathiyakeerthy <khopithankhopi@gmail.com>
2020-08-26 15:48:37 +0530

    second commit

--- Hello.java
+++ Hello.java
@@ -3,6 +3,10 @@ public class Hello{
 	public static void main(String[] args){
 	
 		System.out.println("Hi pissasu");
+		System.out.println("Hi madam");
+		System.out.println("Hi thanuma");
+		System.out.println("Hi kili");
+		System.out.println("Hi sahi");
 
 	}
 
